0c5353c5aeb9824fbcc6ac89198698fdd29bee0c
Felix Ngetich <fellixkn@gmail.com>
2024-02-04 16:01:40 +0300

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,7 @@ if __name__ == "__main__":
     """Print the number of and list of arguments."""
     import sys
     
-    args = sys.argv[1:]
-    count = sum(len(arg.split()) for arg in args)
+    count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
     elif count == 1:
