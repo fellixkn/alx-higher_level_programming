8c812662b71b78882e291b9be8e2350881c07620
Felix Ngetich <fellixkn@gmail.com>
2024-02-04 15:15:37 +0300

    refix

--- 0x04-python-more_data_structures/0-main.py
+++ 0x04-python-more_data_structures/0-main.py
@@ -2,7 +2,7 @@
 square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
 
 matrix = [
-        [1, , 3],
+        [1, 2, 3],
         [4, 5, 6],
         [7, 8, 9]
         ]
